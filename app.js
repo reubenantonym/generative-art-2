173561b62884c745df8a306ab5d412ae5572b5f5
reubenantonym <retouchingr@gmail.com>
2023-08-22 15:14:51 +0300

    Add save the state #20 and restore the state #28

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
 
     context.fillStyle = "black";
+
     /// Placing the box on the canvas
     const x = width * 0.5;
     const y = height * 0.5;
     const w = width * 0.3;
     const h = height * 0.3;
 
+    context.save();
     /// Changing position of  our box
     context.translate(x, y);
     context.rotate(0.3);
@@ -23,6 +25,7 @@ const sketch = () => {
     context.beginPath();
     context.rect(-w * 0.5, -h * 0.5, w, h);
     context.fill();
+    context.restore();
   };
 };
 
