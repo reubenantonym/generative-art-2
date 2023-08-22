66ce953aa4290a0a0bc8e6cbd07f3e259a853f5e
reubenantonym <retouchingr@gmail.com>
2023-08-22 14:51:09 +0300

    create the first box property

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    context.fillStyle = "black";
+
+    const x = 0;
+    const y = 0;
+    const w = width * 0.5;
+    const h = height * 0.5;
+
+    context.beginPath();
+    context.rect(x, y, w, h);
+    context.fill();
   };
 };
 
